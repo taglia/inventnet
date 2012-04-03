a2a9735320b7616735880d00488c58780b9c2194
taglia <taglia@me.com>
2012-04-03 23:10:14 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inventnet::Application.config.secret_token = '22457ea66cbf99ae0468c84a8660e92dfa7b58a09ee60a250bb545ee312a9f8649fada753d5909845e9606107a9718f31e81c40c815a956cbf44ad3a7cad24d0'
