85e40036c8439bed4e988c5ba0fb5787686ef314
taglia <taglia@me.com>
2012-04-06 18:29:11 +0800

    Some eye candy, updated twitter-bootstrap for awesomefont, used simple_format to show text areas

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def nav_link (label, path, options=nil)
     result = "<li"
-    if current_page? path
+    if request.fullpath.rindex path
       result += " class 'active'";
     end
     result += ">"+link_to(label, path, options)+"</li>"
